ba7efb76ae15781b74541a987ce3b13daeb47e94
fengyang <572556127@qq.com>
2020-10-27 16:14:06 +0800

    first commit1027

---
+++ docs/.vuepress/components/Valine.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="vcomments"></div>
+</template>
+
+<script>
+export default {
+  name: 'valine',
+  mounted: function(){
+    // require window 
+    const Valine = require('valine');
+    if (typeof window !== 'undefined') {
+      this.window = window
+      window.AV = require('leancloud-storage')
+      
+    }
+     
+    new Valine({
+      el: '#vcomments' ,
+      appId: '5yDexh0gcXFTRwSYN93e1FlO-gzGzoHsz',// your appId
+      appKey: '3A1nGxssaJSwUNUPgRkzV5Ha', // your appKey
+      notify:true, 
+      verify:false, 
+      avatar:'fy', 
+      placeholder: 'just go go' 
+    });
+  },
+}
+</script>
+
